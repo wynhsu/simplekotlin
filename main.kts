e086ee64afcc7115ac7130ce17affb0d055f5568
wynhsu <wynhsu@uw.edu>
2018-04-05 21:55:35 -0700

    ver. 1

--- main.kts
+++ main.kts
@@ -3,14 +3,91 @@
 println("UW Homework: Simple Kotlin")
 
 // write a "whenFn" that takes an arg of type "Any" and returns a String
+fun whenFn (x: Any): String {
+    when (x) {
+        "Hello" -> return "world"
+        is String -> return "Say what?"
+        0 -> return "zero"
+        1 -> return "one"
+        in 2..10 -> return "low number"
+        is Int -> return "a number"
+        else -> return "I don't understand"
+    }
+}
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
+fun add (a: Int, b: Int): Int {
+    return a + b
+}
+
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
+fun sub (a: Int, b: Int): Int {
+    return a - b
+}
+
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
+fun mathOp (a: Int, b: Int, func: (x: Int, y: Int) -> Int): Int {
+    return func(a, b)
+}
 
 // write a class "Person" with first name, last name and age
+class Person (val firstName: String, val lastName: String, var age: Int) {
+    fun equals(p: Person): Boolean {
+        return  p.firstName == this.firstName && p.lastName == this.lastName && p.age == this.age
+    }
+
+    val debugString: String by lazy { 
+        "[Person firstName:" + this.firstName + " lastName:" + this.lastName + " age:" + this.age + "]"
+    }
+}
 
 // write a class "Money"
+class Money (var amount: Int, var currency: String) {
+    init {
+        if (this.amount < 0)
+            throw Exception("Can't be negative!")
+        else if (!arrayOf("USD", "EUR", "CAN", "GBP").contains(this.currency))
+            throw Exception("Currency unexchangeable")
+    }
+
+    fun convert(to: String): Money {
+        if (this.currency == "USD") {
+            when (to) {
+                "USD" -> return Money(this.amount*1, "USD")
+                "EUR" -> return Money(this.amount*3/2, "USD")
+                "CAN" -> return Money(this.amount*5/4, "USD")
+                "GBP" -> return Money(this.amount*1/2, "USD")
+            }
+        } else if (this.currency == "EUR") {
+            when (to) {
+                "USD" -> return Money(this.amount*2/3, "EUR")
+                "EUR" -> return Money(this.amount*1, "EUR")
+                "CAN" -> return Money(this.amount*5/6, "EUR")
+                "GBP" -> return Money(this.amount*1/3, "EUR")
+            }
+        } else if (this.currency == "CAN") {
+            when (to) {
+                "USD" -> return Money(this.amount*4/5, "CAN")
+                "EUR" -> return Money(this.amount*6/5, "CAN")
+                "CAN" -> return Money(this.amount*1, "CAN")
+                "GBP" -> return Money(this.amount*2/5, "CAN")
+            }
+        } else { // to GBP
+            when (to) {
+                "USD" -> return Money(this.amount*2, "GBP")
+                "EUR" -> return Money(this.amount*3, "GBP")
+                "CAN" -> return Money(this.amount*5/2, "GBP")
+                "GBP" -> return Money(this.amount*1, "GBP")
+            }
+        }
+        return Money(this.amount, this.currency)
+    }
+
+    operator fun plus(a: Money): Money {
+        val converted = a.convert(this.currency)
+        return Money(this.amount + converted.amount, this.currency)
+    }
+}
 
 // ============ DO NOT EDIT BELOW THIS LINE =============
 
